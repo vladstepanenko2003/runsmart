215cf6deb9b538727fa3a7deb75af30d07a55d46
Vlad <vlad.stepanenko.2003@gmail.com>
2022-09-03 12:22:00 +0300

    added carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,3 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1000,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.png"></button>',
-//         adaptiveHeight: true,
-//         responsive: [
-//             {
-//               breakpoint: 992,
-//               settings: {
-//                 arrows: false,
-//                 dots: true
-//               }
-//             }
-//         ]
-//     });
-//   });
-
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -31,4 +13,27 @@ document.querySelector('.prev').addEventListener('click', function () {
 
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
+});
+
+$(document).ready(function(){
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {  //функция будет выполняться для каждого элемента с классом catalog-item__link
+            $(this).on('click', function(e) {  //при клике на один из элементов с классом catalog-item__link будет выполняться функция
+                e.preventDefault();  //говорим браузеру, что мы изменяем его дефолтное поведение
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');  //если у элемента есть данный класс, то он убирается,
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');        //а если нету, то, наоборот, добавляется
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__list');
+
 });
